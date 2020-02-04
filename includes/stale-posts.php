9f7407ea34e8052a6d7d8b882f85b681ce63a335
philcable <phil@happyprime.co>
2020-02-04 10:59:31 -0800

    Introduce a meta box for managing post staleness

--- includes/stale-posts.php
+++ includes/stale-posts.php
@@ -9,6 +9,8 @@ namespace SFS411\Dashboard\Stale_Content;
 
 add_action( 'admin_menu', __NAMESPACE__ . '\add_stale_posts_page' );
 add_filter( 'submenu_file', __NAMESPACE__ . '\stale_posts_submenu_file' );
+add_action( 'add_meta_boxes_knowledge_base', __NAMESPACE__ . '\\add_meta_boxes' );
+add_action( 'save_post_knowledge_base', __NAMESPACE__ . '\\save_post', 10, 2 );
 
 /**
  * Adds the Stale Posts page the the Knowledge Base menu.
@@ -37,4 +39,109 @@ function stale_posts_submenu_file( $submenu_file ) {
 	}
 
 	return $submenu_file;
+}
+
+/**
+ * Adds a meta box for managing post staleness.
+ *
+ * @since 0.3.0
+ *
+ * @param string $post_type
+ */
+function add_meta_boxes() {
+	add_meta_box(
+		'sfs411-staleness-management',
+		'Staleness Management',
+		__NAMESPACE__ . '\display_staleness_management_meta_box',
+		'knowledge_base',
+		'side',
+		'high'
+	);
+}
+
+/**
+ * Returns an array of field values keyed by id.
+ *
+ * @return array Field values keyed by id.
+ */
+function get_stale_in_fields() {
+	return array(
+		'half-year' => '6 months',
+		'year'      => '1 year',
+	);
+}
+
+/**
+ * Displays a meta box used to manage post staleness.
+ *
+ * @since 0.3.0
+ *
+ * @param \WP_Post $post
+ */
+function display_staleness_management_meta_box( $post ) {
+	$stale_in = get_post_meta( $post->ID, '_sfs411_stale_in', true );
+	$stale_by = get_post_meta( $post->ID, '_sfs411_stale_by', true );
+
+	wp_nonce_field( 'sfs411_check_staleness_nonce', 'sfs411-staleness-nonce' );
+
+	if ( $stale_in && $stale_by ) :
+		?>
+			<p>This post is set to be marked as stale on <?php echo esc_html( date( 'F j, Y', strtotime( $stale_by ) ) ); ?>.
+		<?php
+	endif;
+
+	?>
+
+	<p>Mark this post as stale:</p>
+
+	<?php foreach ( get_stale_in_fields() as $id => $value ) : ?>
+	<p>
+		<input
+			type="radio"
+			id="<?php echo esc_attr( $id ); ?>"
+			name="_sfs411_stale_in"
+			value="<?php echo esc_attr( $value ); ?>"
+			<?php checked( $stale_in, $value ); ?>
+		>
+		<label for="half-year">in <?php echo esc_html( $value ); ?></label>
+	</p>
+	<?php endforeach; ?>
+
+	<?php
+}
+
+/**
+ * Saves the meta for tracking the staleness of a post.
+ *
+ * @since 0.3.0
+ *
+ * @param int      $post_id
+ * @param \WP_Post $post
+ */
+function save_post( $post_id, $post ) {
+	if ( ! isset( $_POST['sfs411-staleness-nonce'] ) || ! wp_verify_nonce( $_POST['sfs411-staleness-nonce'], 'sfs411_check_staleness_nonce' ) ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if ( 'auto-draft' === $post->post_status ) {
+		return;
+	}
+
+	if ( isset( $_POST['_sfs411_stale_in'] ) && in_array( $_POST['_sfs411_stale_in'], array_values( get_stale_in_fields() ), true ) ) {
+
+		// Get the current date and modify it by the `_sfs411_stale_in` value.
+		$date = new \DateTime();
+		$stale_by = $date->modify( '+' . $_POST['_sfs411_stale_in'] )->format( 'Y-m-d' );
+
+		update_post_meta( $post_id, '_sfs411_stale_by', $stale_by );
+		update_post_meta( $post_id, '_sfs411_stale_in', $_POST['_sfs411_stale_in'] );
+	}
 }
